c585fa4e639c6daeae1f4ea56b6c55044e04579d
TheUnfocused <33618431+umutpo@users.noreply.github.com>
2024-11-24 14:41:15 -0800

    Added new index

--- run_flask_server.sh
+++ run_flask_server.sh
@@ -31,5 +31,5 @@ if ! [ -d "./custom_nodes/comfyui_controlnet_aux" ]; then
     git clone https://github.com/Fannovel16/comfyui_controlnet_aux.git ./custom_nodes/comfyui_controlnet_aux
 fi
 
-nohup python3 flask_server.py &
+python3 flask_server.py
 deactivate
